d25439a34cb29b0ceb8caf675048cd3de0d519aa
Katie Byers <lobsterkatie@gmail.com>
2015-10-24 12:23:59 -0700

    get_model_info working

--- query.py
+++ query.py
@@ -15,102 +15,102 @@ from model import *
 
 init_app()
 
-# -------------------------------------------------------------------
-# Start here.
+# # -------------------------------------------------------------------
+# # Start here.
 
-# PART 1.5: PLAY WITH SQL TO CHANGE COLUMN NAMES
-    # STEP 1: CREATE A NEW TABLE WITH THE NEW COLUMN NAMES
-    CREATE TABLE Models2 (
-    ...> model_id INTEGER PRIMARY KEY,
-    ...> year INT(4) NOT NULL, 
-    ...> brand_name VARCHAR(50) NOT NULL,
-    ...> model_name VARCHAR(50) NOT NULL
-    ...> );
+# # PART 1.5: PLAY WITH SQL TO CHANGE COLUMN NAMES
+#     # STEP 1: CREATE A NEW TABLE WITH THE NEW COLUMN NAMES
+#     CREATE TABLE Models2 (
+#     ...> model_id INTEGER PRIMARY KEY,
+#     ...> year INT(4) NOT NULL, 
+#     ...> brand_name VARCHAR(50) NOT NULL,
+#     ...> model_name VARCHAR(50) NOT NULL
+#     ...> );
 
-    # STEP 2: COPY DATA FROM OLD TABLE INTO NEW TABLE
-    INSERT INTO Models2 SELECT * FROM Models;
+#     # STEP 2: COPY DATA FROM OLD TABLE INTO NEW TABLE
+#     INSERT INTO Models2 SELECT * FROM Models;
 
-    # STEP 3: DROP OLD TABLE
-    DROP TABLE Models;
+#     # STEP 3: DROP OLD TABLE
+#     DROP TABLE Models;
 
-    # STEP 4: RENAME NEW TABLE TO OLD NAME
-    ALTER TABLE Models2 RENAME TO Models;
+#     # STEP 4: RENAME NEW TABLE TO OLD NAME
+#     ALTER TABLE Models2 RENAME TO Models;
 
-    # STEP 5: DO IT ALL AGAIN WITH THE OTHER TABLE
-    CREATE TABLE Brands2 (
-    ...> brand_id INTEGER PRIMARY KEY,
-    ...> brand_name VARCHAR(50) NOT NULL UNIQUE,
-    ...> yr_founded INT(4),
-    ...> HQ_location VARCHAR(50),
-    ...> yr_discontinued INT(4)
-    ...> );
-    # THEN
-    INSERT INTO Brands2 SELECT * FROM Brands;
-    # THEN
-    DROP TABLE Brands;
-    # AND FINALLY
-    ALTER TABLE Brands2 RENAME TO Brands;
-
-
-# Part 2: Write queries
-
-# Get the brand with the **id** of 8.
-db.session.query(Brand)
-          .filter(Brand.brand_id == 8)
-          .one()
+#     # STEP 5: DO IT ALL AGAIN WITH THE OTHER TABLE
+#     CREATE TABLE Brands2 (
+#     ...> brand_id INTEGER PRIMARY KEY,
+#     ...> brand_name VARCHAR(50) NOT NULL UNIQUE,
+#     ...> yr_founded INT(4),
+#     ...> HQ_location VARCHAR(50),
+#     ...> yr_discontinued INT(4)
+#     ...> );
+#     # THEN
+#     INSERT INTO Brands2 SELECT * FROM Brands;
+#     # THEN
+#     DROP TABLE Brands;
+#     # AND FINALLY
+#     ALTER TABLE Brands2 RENAME TO Brands;
 
 
-# Get all models with the **name** Corvette and the **brand_name** Chevrolet.
-db.session.query(Model)
-          .filter(Model.model_name == "Corvette", 
-                  Model.brand_name == "Chevrolet")
-          .all()
+# # Part 2: Write queries
+
+# # Get the brand with the **id** of 8.
+# db.session.query(Brand)
+#           .filter(Brand.brand_id == 8)
+#           .one()
 
 
-# Get all models that are older than 1960.
-db.session.query(Model)
-          .filter(Model.year < 1960)
-          .all()
-# it should be noted that ascii vs unicode just ate an hour of my life I'll
-# never get back. *sigh*
+# # Get all models with the **name** Corvette and the **brand_name** Chevrolet.
+# db.session.query(Model)
+#           .filter(Model.model_name == "Corvette", 
+#                   Model.brand_name == "Chevrolet")
+#           .all()
 
 
-# Get all brands that were founded after 1920.
-db.session.query(Brand)
-          .filter(Brand.yr_founded > 1920)
-          .all()
+# # Get all models that are older than 1960.
+# db.session.query(Model)
+#           .filter(Model.year < 1960)
+#           .all()
+# # it should be noted that ascii vs unicode just ate an hour of my life I'll
+# # never get back. *sigh*
 
 
-# Get all models with names that begin with "Cor".
-db.session.query(Model)
-          .filter(Model.model_name.like("Cor%"))
-          .all()
+# # Get all brands that were founded after 1920.
+# db.session.query(Brand)
+#           .filter(Brand.yr_founded > 1920)
+#           .all()
 
 
-# Get all brands with that were founded in 1903 and that are not yet discontinued.
-db.session.query(Brand)
-          .filter(Brand.yr_founded == 1903, 
-                  Brand.yr_discontinued.is_(None))
-          .all()
+# # Get all models with names that begin with "Cor".
+# db.session.query(Model)
+#           .filter(Model.model_name.like("Cor%"))
+#           .all()
 
 
-# Get all brands with that are either discontinued or founded before 1950.
-db.session.query(Brand)
-          .filter((Brand.yr_discontinued.isnot(None)) | 
-                  (Brand.yr_founded < 1950))
-          .all()
-# This is ambiguously-worded. Above is the D, or F before 1950 reading.
-# Below is the D or F, before 1950 reading.
-db.session.query(Brand)
-          .filter((Brand.yr_discontinued < 1950) | 
-                  (Brand.yr_founded < 1950))
-          .all()
+# # Get all brands with that were founded in 1903 and that are not yet discontinued.
+# db.session.query(Brand)
+#           .filter(Brand.yr_founded == 1903, 
+#                   Brand.yr_discontinued.is_(None))
+#           .all()
 
 
-# Get any model whose brand_name is not Chevrolet.
-db.session.query(Model)
-          .filter(Model.brand_name != "Chevrolet")
-          .all()
+# # Get all brands with that are either discontinued or founded before 1950.
+# db.session.query(Brand)
+#           .filter((Brand.yr_discontinued.isnot(None)) | 
+#                   (Brand.yr_founded < 1950))
+#           .all()
+# # This is ambiguously-worded. Above is the D, or F before 1950 reading.
+# # Below is the D or F, before 1950 reading.
+# db.session.query(Brand)
+#           .filter((Brand.yr_discontinued < 1950) | 
+#                   (Brand.yr_founded < 1950))
+#           .all()
+
+
+# # Get any model whose brand_name is not Chevrolet.
+# db.session.query(Model)
+#           .filter(Model.brand_name != "Chevrolet")
+#           .all()
 
 
 # Fill in the following functions. (See directions for more info.)
@@ -119,7 +119,22 @@ def get_model_info(year):
     '''Takes in a year, and prints out each model, brand_name, and brand
     headquarters for that year using only ONE database query.'''
 
-    pass
+    results = (db.session.query(Model.model_name,
+                               Model.brand_name,
+                               Brand.HQ_location)
+                         .join(Brand)
+                         .filter(Model.year == year)
+                         .all())
+
+    print "\n\n--------------- {year} ---------------".format(year=year)
+    print "(Brand Model, Brand HQ)"
+
+    for result in results:
+        model, brand, HQ = result
+        result_str = "{brand} {model}, {HQ}"
+        print result_str.format(brand=brand, model=model, HQ=HQ)
+
+    print
 
 def get_brands_summary():
     '''Prints out each brand name, and each model name for that brand
