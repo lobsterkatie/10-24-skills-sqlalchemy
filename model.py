840c143f30fb7f7f0c62c34d22d2d3ea02c17aa5
Katie Byers <lobsterkatie@gmail.com>
2015-10-23 22:53:51 -0700

    data model (classes) created

--- model.py
+++ model.py
@@ -14,14 +14,50 @@ db = SQLAlchemy()
 
 class Model(db.Model):
 
-    __tablename__ = "models"
-    pass
+    __tablename__ = "Models"
+
+    model_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    year = db.Column(db.Integer, nullable=False)
+    brand_name = db.Column(db.String(50), db.ForeignKey("Brand.brand_name"),
+                           nullable=True) #QUESTION FOR MEGGIE: NULL? DEFAULT?
+    model_name = db.Column(db.String(50), nullable=False)
+
+    brand = db.relationship("Brand",
+                            backref=db.backref("Models", order_by=brand_name))
+
+
+    def __repr__(self):
+        """Make printing the object useful"""
+
+        repr_string = ("<Model model_id: {id}, year: {year}, " +
+                       "brand_name: {brand}, model_name: {model}>")
+        print repr_string.format(id=self.model_id, year=self.year,
+                                 brand=self.brand_name, model=self.model_name)
 
 
 class Brand(db.Model):
 
-    __tablename__ = "brands"
-    pass
+    __tablename__ = "Brands"
+
+    brand_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    brand_name = db.Column(db.String(50), nullable=False, unique=True)
+    yr_founded = db.Column(db.Integer, nullable=True)
+    HQ_location = db.Column(db.String(50), nullable=True)
+    #QUESTION FOR MEGGIE: STYLE-WISE, OKAY TO USE CAPS IN VAR NAMES IF IT'S CLEAR
+    #THEY'RE NOT CONSTANTS, JUST BECAUSE OF ACRONYMS?
+    yr_discontinued = db.Column(db.Integer, nullable=True)
+
+
+    def __repr__(self):
+        """Make printing the object useful"""
+
+        repr_string = ("Brand brand_id: {id}, brand_name: {name}, " +
+                       "yr_founded: {founded}, HQ_location: {HQ}, " +
+                       "yr_discontinued: {ended}")
+        print repr_string.format(id=self.brand_id, name=self.brand_name,
+                                 founded=self.yr_founded, HQ=self.HQ_location,
+                                 ended=self.yr_discontinued)
+
 
 # End Part 1
 ##############################################################################
